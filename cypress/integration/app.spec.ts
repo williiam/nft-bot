df86cda96c516ee41fc2bf9c235436dcf1938e5d
William <william1006wei@Gmail.com>
2022-07-03 17:20:44 +0800

    change cypress test case

--- cypress/integration/app.spec.ts
+++ cypress/integration/app.spec.ts
@@ -1,18 +1,18 @@
 // cypress/integration/app.spec.js
 
 describe('Navigation', () => {
-  it('should navigate to the about page', () => {
+  it('should navigate to the main page', () => {
     // Start from the index page
     cy.visit('http://localhost:3000/')
 
     // Find a link with an href attribute containing "about" and click it
-    cy.get('a[href*="about"]').click()
+    cy.get('a [id="text"]').click()
 
     // The new url should include "/about"
-    cy.url().should('include', '/about')
+    cy.url().should('include', '/main')
 
     // The new page should contain an h1 with "About page"
-    cy.get('h1').contains('About Page')
+    cy.get('h1').contains('NFT.bot')
   })
 })
 
